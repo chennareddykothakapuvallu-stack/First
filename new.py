87042e766bd777447d38a26bd5e270fea2be0b0e
Chenna Reddy <chennareddykothakapuvallu@gmail.com>
2025-10-31 11:51:43 +0530

    initial commit

---
+++ new.py
@@ -0,0 +1,10 @@
+# Small Python Program to Check Even or Odd
+
+# Take input from the user
+num = int(input("Enter a number: "))
+
+# Check if the number is even or odd
+if num % 2 == 0:
+    print(f"{num} is Even")
+else:
+    print(f"{num} is Odd")
